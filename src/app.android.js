208696f564d9fbed6ab89ec0b2a7d104a1156321
Daniel Zlotin <danielzl@wix.com>
2016-08-22 14:29:47 +0300

    welcome msg

--- src/app.android.js
+++ src/app.android.js
@@ -15,7 +15,7 @@ class template extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>{'Welcome to React Native!'}</Text>
+        <Text style={styles.welcome}>{'Welcome to React Native! 0.25.1'}</Text>
         <Text style={styles.instructions}>{'To get started, edit index.android.js'}</Text>
         <Text style={styles.instructions}>{'Shake or press menu button for dev menu'}</Text>
       </View>
